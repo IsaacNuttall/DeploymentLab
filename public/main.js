077df974a7e1b96067527c55c79f830a10641f10
Ubuntu <ubuntu@ip-172-31-3-115.us-east-2.compute.internal>
2023-04-07 04:51:51 +0000

    publichost

---
+++ public/main.js
@@ -0,0 +1,86 @@
+const complimentBtn = document.getElementById("complimentButton")
+const fortuneBtn = document.querySelector("#fortuneButton")
+const goalInput = document.querySelector('#newGoal')
+const addGoalBtn = document.querySelector('#addGoal')
+const deleteInput = document.querySelector('#goalId')
+const deleteGoalBtn = document.querySelector('#deleteButton')
+const updateInput = document.querySelector('#replacementGoal')
+const idToUpdate = document.querySelector('#replacementid')
+const updateBtn = document.querySelector('#updateButton')
+
+const baseURL = 'http://18.220.53.166/api'
+
+const getCompliment = () => {
+    axios.get("http://18.220.53.166/api/compliment/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+
+const getFortune = () => {
+    axios.get("http://18.220.53.166/api/fortune/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+
+const addGoal = () => {
+
+    let bodyObj = {
+        goal: goalInput.value
+    }
+
+    axios.post(`${baseURL}/goal`, bodyObj)
+        .then((res) => {
+            console.log(res.data)
+            })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    const deleteGoal = () => {
+            
+        let idToDelete = deleteInput.value
+            
+        axios.delete(`${baseURL}/goal/${idToDelete}`)
+        .then((res) => {
+             console.log(res.data)
+                    })
+                .catch((err) => {
+                console.log(err)
+                    })
+            }
+
+const updateGoal = () => {
+    console.log('Goal updated')
+
+    let bodyObj2 = {
+        goal: updateInput.value
+    }
+console.log(updateInput.value)
+console.log(idToUpdate.value)
+
+axios.put(`${baseURL}/goal/${idToUpdate.value}`, bodyObj2)
+
+.then((res) => {
+    console.log(res.data)
+    })
+.catch((err) => {
+    console.log(err)
+})
+.finally(() => console.log('final'))
+
+}
+
+complimentBtn.addEventListener('click', getCompliment)
+
+fortuneBtn.addEventListener('click', getFortune)
+
+addGoalBtn.addEventListener('click', addGoal)
+
+deleteBtn.addEventListener('click', deleteGoal)
+
+dateBtn.addEventListener('click', updateGoal)
